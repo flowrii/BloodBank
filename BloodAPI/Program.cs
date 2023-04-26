66d0d23a6bd32aa9ee1e6b7a25c85ab41a86dc70
floriniftene <floriniftene01@gmail.com>
2023-04-26 20:16:43 +0300

    up

--- BloodAPI/Program.cs
+++ BloodAPI/Program.cs
@@ -8,8 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
